d0c0dfa721590a0c44080135ecccfa64c81bcf64
Rakshith <rakshithlokesha@gmail.com>
2023-04-18 14:26:43 +0530

    intial

---
+++ src/main/java/com/example/pusherrevamp/PusherRevampApplication.java
@@ -0,0 +1,13 @@
+package com.example.pusherrevamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PusherRevampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PusherRevampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pusherrevamp/PusherRevampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pusherrevamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PusherRevampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
